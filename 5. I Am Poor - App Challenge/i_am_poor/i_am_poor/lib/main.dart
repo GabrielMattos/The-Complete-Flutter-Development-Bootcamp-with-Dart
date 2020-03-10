3c4be7df229e5ec23515b97837be981885043b42
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-10 11:07:47 -0300

    2. What are App Challenges

---
+++ 5. I Am Poor - App Challenge/i_am_poor/i_am_poor/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Poor'),
+          backgroundColor: Colors.lightGreen,
+        ),
+        backgroundColor: Colors.green,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/rockVector.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
