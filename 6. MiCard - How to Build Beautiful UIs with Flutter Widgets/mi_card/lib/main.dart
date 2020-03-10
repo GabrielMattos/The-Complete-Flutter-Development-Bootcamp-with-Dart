7b71c8c030c1d65b08dbee55d3495bd0e66152bc
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-10 14:44:03 -0300

    6. Tapping into Widget Properties

--- 6. MiCard - How to Build Beautiful UIs with Flutter Widgets/mi_card/lib/main.dart
+++ 6. MiCard - How to Build Beautiful UIs with Flutter Widgets/mi_card/lib/main.dart
@@ -12,24 +12,20 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 30.0,
-                color: Colors.white,
-                child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/imagem.jpg'),
+                backgroundColor: Colors.red,
               ),
-              SizedBox(
-                width: 30.0,
-              ),
-              Container(
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                color: Colors.red,
-                child: Text('Container 3'),
+              Text(
+                'Gabriel Mattos',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ],
           ),
