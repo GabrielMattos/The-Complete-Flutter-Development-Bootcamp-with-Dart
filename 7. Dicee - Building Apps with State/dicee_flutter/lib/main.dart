93585e0719c5658f30b1619f6a59bae508133da5
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-12 13:36:46 -0300

    9. Stateful vs. Stateless Widgets - What's the Difference

--- 7. Dicee - Building Apps with State/dicee_flutter/lib/main.dart
+++ 7. Dicee - Building Apps with State/dicee_flutter/lib/main.dart
@@ -15,20 +15,34 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+/*class  extends StatelessWidget { //Immutable
   @override
   Widget build(BuildContext context) {
+    return Container();
+  }
+}*/
 
-    var leftDiceNumber = 1;
-    var rightDiceNumber = 2;
-    
+class DicePage extends StatefulWidget { //Mutable
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 2;
+
+  @override
+  Widget build(BuildContext context) {
     return Center(
         child: Row(
       children: <Widget>[
         Expanded(
           child: FlatButton(
-            onPressed: () { //void callback
-
+            onPressed: () {
+              //void callback
+              setState(() {
+                leftDiceNumber = 5;
+              });
             },
             child: Image.asset('images/dice$leftDiceNumber.png'),
           ),
@@ -36,7 +50,9 @@ class DicePage extends StatelessWidget {
         Expanded(
           child: FlatButton(
             onPressed: () {
-
+              setState(() {
+                rightDiceNumber = 3;
+              });
             },
             child: Image.asset('images/dice$rightDiceNumber.png'),
           ),
